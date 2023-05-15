293d98c120aa6368a25bd8d8b8a3cf9ad31db43b
Tyler Gaudin <tylermerrek@gmail.com>
2023-05-15 17:33:58 -0500

    B2. Include details for each vacation

--- app/src/main/java/android/schedulertyler/tripwizard/entities/Vacation.java
+++ app/src/main/java/android/schedulertyler/tripwizard/entities/Vacation.java
@@ -1,2 +1,67 @@
-package android.schedulertyler.tripwizard.entities;public class Vacation {
+package android.schedulertyler.tripwizard.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity(tableName = "vacations")
+public class Vacation {
+    @PrimaryKey(autoGenerate = true)
+    private int vacationID;
+    private String vacationTitle;
+    private String lodging;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Vacation(int vacationID, String vacationTitle, String lodging, LocalDate startDate, LocalDate endDate) {
+        this.vacationID = vacationID;
+        this.vacationTitle = vacationTitle;
+        this.lodging = lodging;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Vacation() {
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
+
+    public String getVacationTitle() {
+        return vacationTitle;
+    }
+
+    public void setVacationTitle(String vacationTitle) {
+        this.vacationTitle = vacationTitle;
+    }
+
+    public String getLodging() {
+        return lodging;
+    }
+
+    public void setLodging(String lodging) {
+        this.lodging = lodging;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
