6751abc90cd9c26854de0019eb37da05da54d5be
Tyler Gaudin <tylermerrek@gmail.com>
2023-05-15 17:48:09 -0500

    B4. Include details for each excursion

---
+++ app/src/main/java/android/schedulertyler/tripwizard/entities/Excursion.java
@@ -0,0 +1,60 @@
+package android.schedulertyler.tripwizard.entities;
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import java.time.LocalDate;
+
+@Entity(tableName = "Excursions")
+public class Excursion {
+
+    @PrimaryKey(autoGenerate = true)
+    private int excursionID;
+
+    private String excursionTitle;
+    private LocalDate excursionDate;
+    private int vacationID;
+
+    public Excursion(int excursionID, String excursionTitle, LocalDate excursionDate, int vacationID) {
+        this.excursionID = excursionID;
+        this.excursionTitle = excursionTitle;
+        this.excursionDate = excursionDate;
+        this.vacationID = vacationID;
+    }
+
+    public Excursion() {
+    }
+
+    public int getExcursionID() {
+        return excursionID;
+    }
+
+    public void setExcursionID(int excursionID) {
+        this.excursionID = excursionID;
+    }
+
+    public String getExcursionTitle() {
+        return excursionTitle;
+    }
+
+    public void setExcursionTitle(String excursionTitle) {
+        this.excursionTitle = excursionTitle;
+    }
+
+    public LocalDate getExcursionDate() {
+        return excursionDate;
+    }
+
+    public void setExcursionDate(LocalDate excursionDate) {
+        this.excursionDate = excursionDate;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
+}
