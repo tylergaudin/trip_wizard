9277e51cc7f846f8a7b2334364b6c5a5c5381a99
Tyler Gaudin <tylermerrek@gmail.com>
2023-05-15 16:10:52 -0500

    Add details files

---
+++ app/src/main/java/android/schedulertyler/tripwizard/UI/excursionDetails.java
@@ -0,0 +1,15 @@
+package android.schedulertyler.tripwizard.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.schedulertyler.tripwizard.R;
+
+public class excursionDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excursion_details);
+    }
+}
---
+++ app/src/main/java/android/schedulertyler/tripwizard/UI/vacationDetails.java
@@ -0,0 +1,15 @@
+package android.schedulertyler.tripwizard.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.schedulertyler.tripwizard.R;
+
+public class vacationDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vacation_details);
+    }
+}
